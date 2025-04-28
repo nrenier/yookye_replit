2d15249402890c28fe4d77cecd48804d3724cc4c
nicola.renier <nicola.renier@gmail.com>
2025-04-28 07:37:40 +0000

    fix auth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from './hooks/auth-provider';
+import { AuthProvider } from './hooks/use-auth'; // Corrected import path
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
